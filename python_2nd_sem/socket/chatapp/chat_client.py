e1eb302ed984410aed0d43b01da84e37639d2489
sanskaryo <sanskartodwal@gmail.com>
2024-05-30 18:56:32 +0530

    minor

--- python_2nd_sem/socket/chatapp/chat_client.py
+++ python_2nd_sem/socket/chatapp/chat_client.py
@@ -1,1 +1,14 @@
 
+# import threading
+# counter = 0
+# def increment():
+# global counter
+# for _ in range(1000):
+# counter += 1
+# thread1 = threading.Thread(target=increment)
+# thread2 = threading.Thread(target=increment)
+# thread1.start()
+# thread2.start()
+# thread1.join()
+# thread2.join()
+# print("Counter:", counter)
